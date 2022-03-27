1be6ce61475ae95b439493878c8b43f281260050
Gonzalo <gonzalo.escamilla@nimblegiant.com>
2022-03-27 11:52:51 -0300

    Simple movement system added. Also, a test tileset was added.

---
+++ StepingMars/Assets/_Project/Scripts/Character/Character.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace StepingMars._Project.Scripts.Character
+{
+    // This Class is going to follow the Facade design pattern.
+    public class Character : MonoBehaviour
+    {
+        [SerializeField] private CharacterMovement _characterMovement;
+        [SerializeField] private Rigidbody2D _rigidbody2D;
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        
+        void Awake()
+        {
+            //Initializing The Character
+            _characterMovement.Init(_rigidbody2D);
+        }
+
+        private void Update()
+        {
+            ProcessInput();
+        }
+
+        private void ProcessInput()
+        {
+            Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+            _characterMovement.Move(movementVector);
+            FlipSpriteIfNeeded(movementVector);
+        }
+
+        private void FlipSpriteIfNeeded(Vector2 movementVector)
+        {
+            float dotProductWithRightVector = Vector2.Dot(movementVector.normalized, Vector2.right);
+            if (dotProductWithRightVector == 0)
+                return;
+            
+            _spriteRenderer.flipX = dotProductWithRightVector < 0;
+        }
+    }
+}
---
+++ StepingMars/Assets/_Project/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace StepingMars._Project.Scripts.Character
+{
+    public class CharacterMovement: MonoBehaviour
+    {
+        [SerializeField] private float _movementSpeed;
+        private Rigidbody2D _rigidbody2D;
+        
+        public void Init(Rigidbody2D rigidbody2D)
+        {
+            _rigidbody2D = rigidbody2D;
+        }
+
+        public void Move(Vector2 movementVector)
+        {
+            _rigidbody2D.velocity = movementVector.normalized * _movementSpeed;
+        }
+    }
+}
